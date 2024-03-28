f748e1c8ff508e89cf2224e0e69f2c7ea2829ebf
natalieroman <natalie.roman@northwestern.edu>
2024-03-28 15:47:16 -0500

    Created custom dialpad for conference call

--- src/components/DisplayArea.js
+++ src/components/DisplayArea.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useState, useEffect } from "react";
 import { Button, Row, Col} from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
+import CModal from './CModal';
 import Opad from './Opad';
 
 function DisplayArea({sourceSelected, displayJoin, side, showAnnotationJoin, showFullScreenJoin,
@@ -156,14 +157,87 @@ function DisplayArea({sourceSelected, displayJoin, side, showAnnotationJoin, sho
         case 'ConfCall':
             message = <span>
                 <p>Select the button below to dial your number.</p>
-                <Button className="btn btn-info rounded-pill border-0 px-3 mt-3 font-size-3 font-size-4-xl">Conference Call</Button>
-                <CModal show={confCallClicked} onHide={handleCloseConfCallModal} title="Conference Call">
-                    <ch5-keypad 
-                        showExtraButton="true" 
-                        type="primary" 
-                        shape="circle">
-                    </ch5-keypad>
-                </CModal>
+                <Button 
+                className="btn btn-info rounded-pill border-0 px-3 mt-3 font-size-3 font-size-4-xl"
+                onClick={handleShowConfCallModal}
+                >Conference Call</Button>
+                {/* Conference Call Modal */}
+                <Modal show={confCallClicked} onHide={handleCloseConfCallModal} fullscreen={fullscreen}>
+                    <Modal.Header closeButton className="pb-0">
+                        <Modal.Title>
+                            <h1 className="font-size-5 font-size-6-xl"><button type="button" className="border-0 text-dark"
+                                onClick={handleCloseConfCallModal}><i className="bi bi-arrow-left"></i></button>Conference Call</h1>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="font-size-2 font-size-3-xl p-0">
+                        <div className='container-fluid text-center pt-3 pt-xl-5'>
+                            <div className="d-flex flex-wrap col-3 col-xl-3 justify-content-around mx-auto">
+                                <div className="d-flex flex-row col-12 justify-content-between">
+                                <div className="col-10">
+                                    <input className="form-control border-0 rounded-pill bg-gray-300 text-muted text-center font-size-1 font-size-3-xl p-2 mb-3"
+                                    placeholder='847-555-5555' />
+                                </div>
+                                <div className="col pt-2">
+                                <i className="bi bi-backspace-fill"></i>
+                                </div>
+                                </div>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">1</span>
+                                    <span className="d-block font-size-1" style={{height: 'var(--font-size-2'}}></span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">2</span>
+                                    <span className="d-block font-size-1 font-size-2-xl">ABC</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">3</span>
+                                    <span className="d-block font-size-1 font-size-2-xl">DEF</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">4</span>
+                                    <span className="d-block font-size-1 font-size-2-xl">GHI</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">5</span>
+                                    <span className="d-block font-size-1 font-size-2-xl">JKL</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">6</span>
+                                    <span className="d-block font-size-1 font-size-2-xl">MNO</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">7</span>
+                                    <span className="d-block font-size-1 font-size-2-xl">PQRS</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">8</span>
+                                    <span className="d-block font-size-1 font-size-2-xl">TUV</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">9</span>
+                                    <span className="d-block font-size-1 font-size-2-xl">WXYZ</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">*</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">0</span>
+                                    <span className="d-block font-size-1 font-size-2-xl">+</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">#</span>
+                                </Button>
+                                <Button className="btn rounded-circle border-0 bg-gray-300 text-dark p-0 mb-2 dialpadButton">
+                                    <span className="d-block fw-bold font-size-4 font-size-5-xl">
+                                        <i className="bi bi-telephone-fill"></i>
+                                    </span>
+                                </Button>
+                                
+                            </div>
+                        </div>
+                    </Modal.Body>
+                </Modal>
+                {/* /Conference Call Modal */}
             </span>;
             break;
         case 'DocCam':
