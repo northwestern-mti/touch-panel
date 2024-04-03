8cccb4aea0f1ee342dfedb0adf8c7ba3524dd052
natalieroman <natalie.roman@northwestern.edu>
2024-04-03 11:07:36 -0500

    Updated microphone button background color to match other buttons

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -369,8 +369,7 @@ const handleNewCamNameChange = (event) => {
             </div>
             <div className="col-12 text-center mb-3 mb-xl-5">
               <button type="button"
-                className={`d-flex align-items-center border-0 rounded-circle text-center text-dark mx-auto mb-3 mb-xl-4 muteIcon ${isMicMuted ? 'bg-info' : ''}`}
-                style={{ backgroundColor: '#D5D5D5' }}
+                className={`d-flex align-items-center border-0 rounded-circle text-center text-dark mx-auto mb-3 mb-xl-4 muteIcon ${isMicMuted ? 'bg-info' : 'bg-gray-300'}`}
                 onClick={toggleMicMute}>
                 <i
                   className={`d-inline-block bi font-size-5 font-size-5-xl mx-auto ${isMicMuted ? 'bi-mic-mute-fill text-white' : 'bi-mic-fill'}`}
@@ -380,8 +379,7 @@ const handleNewCamNameChange = (event) => {
             </div>
             <div className="col-12 text-center">
               <button type="button"
-                className={`d-flex align-items-center border-0 rounded-circle text-center text-dark mx-auto mb-3 mb-xl-4 muteIcon ${isCeilingMicMuted ? 'bg-info' : ''}`}
-                style={{ backgroundColor: '#D5D5D5' }}
+                className={`d-flex align-items-center border-0 rounded-circle text-center text-dark mx-auto mb-3 mb-xl-4 muteIcon ${isCeilingMicMuted ? 'bg-info' : 'bg-gray-300'}`}
                 onClick={toggleCeilingMicMute}>
                 <i
                   className={`d-inline-block bi font-size-5 font-size-5-xl mx-auto ${isCeilingMicMuted ? 'bi-mic-mute-fill text-white' : 'bi-mic-fill'}`}
