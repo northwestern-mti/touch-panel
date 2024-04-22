4148e07d49694c644e69a055d4be9ee37b7230e0
Nelson Kagimbi <NelsonKagimbi2023@u.northwestern.edu>
2024-04-22 12:11:52 -0500

    Add ellipsis to truncate camera and preset names

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -77,6 +77,7 @@ function BottomBar ({programStarted, setProgramStarted}) {
       });
       return value;
     }));
+    console.log('array of cam names', camNames)
     
   }, [numCameras, numOfPresets, tempCamName, tempPresetName])
 
