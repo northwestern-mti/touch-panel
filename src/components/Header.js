6b4bf342161012b8c010a422b624e46345fee999
Nelson Kagimbi <NelsonKagimbi2023@u.northwestern.edu>
2024-04-23 15:33:14 -0500

    Add logic for backspace and fix input placeholder issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -159,7 +159,6 @@ function Header(){
         console.log('current index is', currIdx)
     }
     const handleRoomNameChange = (event) => {
-        console.log("input is",event.target.value)
         setConfigRoomName(event.target.value);
     }
     const handleIpChange = (event) => {
