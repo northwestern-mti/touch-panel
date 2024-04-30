17fabee38b40911d20454e88de59a8b5afea66dd
natalieroman <natalie.roman@northwestern.edu>
2024-04-30 11:55:43 -0500

    Fixed audio status text spacing

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -278,7 +278,7 @@ const popover = (
           </button>}
         {/* Audio Statuses */}
         <div className="col h-100 border-0 pt-2 pb-0 px-1">
-          <div className="d-flex col-11 justify-content-start mb-0">
+          <div className="d-flex col-11 justify-content-start mb-0 mx-auto">
             <div className="col-9 font-size-0 font-size-2-xl m-0 p-0">Presentation Audio</div>
             <div className="col-3 text-center">
               <div
@@ -288,7 +288,7 @@ const popover = (
             </div>
           </div>
           {hasMics &&
-          <div className="d-flex col-11 justify-content-start mb-0">
+          <div className="d-flex col-11 justify-content-start mb-0 mx-auto">
             <div class="col-9 font-size-0 font-size-2-xl p-0 m-0">
               Microphones
             </div>
@@ -300,7 +300,7 @@ const popover = (
             </div>
           </div>}
           {hasCeilingMics && 
-          <div className="d-flex col-11 justify-content-start mb-0">
+          <div className="d-flex col-11 justify-content-start mb-0 mx-auto">
             <div className="col-9 font-size-0 font-size-2-xl p-0 m-0">
               Ceiling Mics
             </div>
